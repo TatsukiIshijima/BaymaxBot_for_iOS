5370ae2bedf01886723112154298ef98631a2f21
TatsukiIshijima <tatsuki445@gmail.com>
2018-06-30 11:20:03 +0900

    入力バーのスタイル変更

--- BaymaxBot/ViewController.swift
+++ BaymaxBot/ViewController.swift
@@ -20,6 +20,7 @@ class ViewController: MessagesViewController, UNUserNotificationCenterDelegate {
     let baymaxSender = Sender(id: "111111", displayName: "ベイマックス")
     let userAvator = Avatar(image: UIImage(named: "img_default"), initials: "ユーザー")
     let baymaxAvator = Avatar(image: UIImage(named: "img_baymax"), initials: "ベイマックス")
+    let defaultBlue = UIColor(red: 0, green: 122 / 255, blue: 1, alpha: 1)
     var messageList: [MessageModel] = []
     
     override func viewDidLoad() {
@@ -52,7 +53,7 @@ class ViewController: MessagesViewController, UNUserNotificationCenterDelegate {
         messagesCollectionView.messageCellDelegate = self
         messageInputBar.delegate = self
         
-        messageInputBar.sendButton.tintColor = UIColor(red: 69/255, green: 193/255, blue: 89/255, alpha: 1)
+        makeInputBar()
         scrollsToBottomOnKeybordBeginsEditing = true // default false
         maintainPositionOnKeyboardFrameChanged = true // default false
         
@@ -79,6 +80,75 @@ class ViewController: MessagesViewController, UNUserNotificationCenterDelegate {
     @objc func tapSettingButton() {
         let token = Messaging.messaging().fcmToken
         print("FCM token: \(token ?? "")")
+    }
+    
+    // 入力バー作成
+    func makeInputBar() {
+        messageInputBar.backgroundView.backgroundColor = .white
+        messageInputBar.isTranslucent = false
+        messageInputBar.inputTextView.backgroundColor = .clear
+        messageInputBar.inputTextView.layer.borderWidth = 0
+        let items = [
+            makeInputBarButton(named: "ic_camera").onTextViewDidChange { button, textView in
+                button.isEnabled = textView.text.isEmpty
+            },
+            makeInputBarButton(named: "ic_image").onSelected {
+                $0.tintColor = self.defaultBlue
+            },
+            makeInputBarButton(named: "ic_voice").onSelected {
+                $0.tintColor = self.defaultBlue
+            },
+            messageInputBar.sendButton
+                .configure {
+                    $0.layer.cornerRadius = 8
+                    $0.layer.borderWidth = 1.5
+                    $0.layer.borderColor = $0.titleColor(for: .disabled)?.cgColor
+                    $0.setTitleColor(.white, for: .normal)
+                    $0.setTitleColor(.white, for: .highlighted)
+                    $0.setSize(CGSize(width: 52, height: 30), animated: true)
+                    $0.title = "送信"
+                    $0.messageInputBar?.inputTextView.placeholderLabel.text = "Aa"
+                }.onDisabled {
+                    $0.layer.borderColor = $0.titleColor(for: .disabled)?.cgColor
+                    $0.backgroundColor = .white
+                }.onEnabled {
+                    $0.backgroundColor = self.defaultBlue
+                    $0.layer.borderColor = UIColor.clear.cgColor
+                }.onSelected {
+                    // We use a transform becuase changing the size would cause the other views to relayout
+                    $0.transform = CGAffineTransform(scaleX: 1.2, y: 1.2)
+                }.onDeselected {
+                    $0.transform = CGAffineTransform.identity
+                }
+        ]
+        items.forEach { $0.tintColor = .lightGray }
+        
+        // We can change the container insets if we want
+        messageInputBar.inputTextView.textContainerInset = UIEdgeInsets(top: 8, left: 0, bottom: 8, right: 0)
+        messageInputBar.inputTextView.placeholderLabelInsets = UIEdgeInsets(top: 8, left: 5, bottom: 8, right: 5)
+        
+        // Since we moved the send button to the bottom stack lets set the right stack width to 0
+        messageInputBar.setRightStackViewWidthConstant(to: 0, animated: true)
+        
+        // Finally set the items
+        messageInputBar.setStackViewItems(items, forStack: .bottom, animated: true)
+    }
+    
+    // 入力バー内のボタン作成
+    func makeInputBarButton(named: String) -> InputBarButtonItem {
+        return InputBarButtonItem()
+            .configure{
+                $0.spacing = .fixed(10)
+                $0.image = UIImage(named: named)?.withRenderingMode(.alwaysTemplate)
+                $0.setSize(CGSize(width: 30, height: 30), animated: true)
+            }.onSelected {
+                $0.tintColor = self.defaultBlue
+            }.onDeselected {
+                $0.tintColor = UIColor.lightGray
+            }.onTouchUpInside { _ in
+                print("Item tapped")
+                // TODO:画像や音声認識の結果をmessageListに追加したらreload()すれば送信したこととなる？
+            }
     }
 }
 
@@ -164,7 +234,7 @@ extension ViewController: MessageInputBarDelegate {
                 messageList.append(imageMessage)
                 messagesCollectionView.insertSections([messageList.count - 1])
             } else if let text = component as? String {
-                let attributedText = NSAttributedString(string: text, attributes: [.font: UIFont.systemFont(ofSize: 15), .foregroundColor: UIColor.blue])
+                let attributedText = NSAttributedString(string: text, attributes: [.font: UIFont.systemFont(ofSize: 15), .foregroundColor: defaultBlue])
                 let message = MessageModel(attributedText: attributedText, sender: currentSender(), messageId: UUID().uuidString, sentDate: Date())
                 messageList.append(message)
                 messagesCollectionView.insertSections([messageList.count - 1])
